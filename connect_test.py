7328813868b1fb51fc991def767a6f01bc736c5a
Catangel007 <162627524+Catangel007@users.noreply.github.com>
2025-10-23 12:07:07 +0100

    New venv

---
+++ connect_test.py
@@ -0,0 +1,49 @@
+import mysql.connector
+from mysql.connector import Error
+
+def execute_safe_sql(cursor, query):
+    """Check query before running it """
+    lower_query = query.lower().strip()
+
+    if lower_query.startswith("select") or lower_query.startwith("show"):
+        print(" Error : 'SELECT' and 'SHOW' statements are not allowed.")
+        return
+
+    try:
+        cursor.execute(query)
+        print(f"✅ Executed: {query}")
+    except Error as e:
+        print(f"⚠️ MySQL Error while executing query: {e}")
+
+def create_database():       
+    try:
+            connection = mysql.connector.connect(
+                    host = "127.0.0.1",
+                    port = 3306,
+                    user = "root",
+                    password = "0404Vera#"
+                    )
+
+
+            if connection.is_connected():
+                cursor = connection.cursor()
+                cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+                cursor.execute("USE alx_book_store")
+                print("Database 'alx_book_store' created successfully!")
+                 # Test the rule:
+                execute_safe_sql(cursor, "SELECT * FROM test_table;")   # blocked
+                execute_safe_sql(cursor, "SHOW DATABASES;")             # blocked
+                execute_safe_sql(cursor, "CREATE TABLE IF NOT EXISTS test_table (name VARCHAR(50));")  # allowed
+                execute_safe_sql(cursor, "INSERT INTO test_table (name) VALUES ('Vera');")             # allowed
+
+                connection.commit()
+    except Error as e:
+        print("Error while connecting to MySQL: ", e)  
+
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+            print("Connection closed.")
+
+      
